e6c388e3100ceaa2f8893fe8b2835669de0dcb48
GobsRuiz <gbibisruiz@hotmail.com>
2024-10-28 10:39:35 -0300

    guides section

---
+++ src/scripts/components/header.js
@@ -0,0 +1,38 @@
+const header = {
+    parent: document.getElementById("header"),
+    nav: document.querySelector(".header__container__nav"),
+    links: document.querySelectorAll(".header__container__nav__link"),
+
+    clickLink(){
+    },
+
+    scroll(){
+        window.addEventListener("scroll", (e) => {
+            if(window.scrollY >= (innerWidth > 991 ? 80 : 20)){
+                this.parent.classList.add("header--scroll")
+            }
+            else{
+                this.parent.classList.remove("header--scroll")
+            }
+        });
+    },
+
+    mobileMenu(){
+        const menuBtn = document.querySelector(".header__container__menuBtn")
+
+        menuBtn.addEventListener("click", () => {
+            menuBtn.classList.toggle("header__container__menuBtn--close")
+            this.parent.classList.toggle("header--openMenu")
+        })
+    },
+
+    init(){
+        this.clickLink()
+        this.scroll()
+
+        if(innerWidth <= 991) this.mobileMenu()
+    },
+}
+setTimeout(() => {
+    header.init()
+}, 200);
