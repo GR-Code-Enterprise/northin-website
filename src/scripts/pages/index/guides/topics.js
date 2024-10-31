ba91970886b825d6d66df7a543cb72186c23fe66
GobsRuiz <gbibisruiz@hotmail.com>
2024-10-31 19:29:09 -0300

    mobile

--- src/scripts/pages/index/guides/topics.js
+++ src/scripts/pages/index/guides/topics.js
@@ -1,23 +1,32 @@
 function topics() {
-    const questions = document.querySelectorAll('.guides__container__topics__questions__question')
-    const answers = document.querySelectorAll(".guides__container__topics__answers__answer")
+    const questions = document.querySelectorAll('.guides__container__topics__topic__question')
+    const answers = document.querySelectorAll(".guides__container__topics__topic__answer")
 
     questions.forEach((question, index) => {
         question.addEventListener("click", (e) => {
             let timeout = 0
-            const activeElement = document.querySelector(".guides__container__topics__questions__question--show")
+            const activeElement = document.querySelector(".guides__container__topics__topic__question--show")
+
+            if(activeElement){
+                timeout = 150
+
+                activeElement.classList.remove("guides__container__topics__topic__question--show")
+                document.querySelector(".guides__container__topics__topic__answer--show").classList.remove("guides__container__topics__topic__answer--show")
+
+                if(innerWidth < 991){
+                    activeElement.parentNode.style.height = "58px"
+                }
+            }
 
             if(question == activeElement) return
 
-            if(activeElement){
-                timeout = 600
-                activeElement.classList.remove("guides__container__topics__questions__question--show")
-                document.querySelector(".guides__container__topics__answers__answer--show").classList.remove("guides__container__topics__answers__answer--show")
-            }
-
             setTimeout(() => {
-                question.classList.add("guides__container__topics__questions__question--show")
-                answers[index].classList.add("guides__container__topics__answers__answer--show") 
+                question.classList.add("guides__container__topics__topic__question--show")
+                answers[index].classList.add("guides__container__topics__topic__answer--show") 
+
+                if(innerWidth < 991){
+                    question.parentNode.style.height = (question.parentNode.offsetHeight + answers[index].offsetHeight + 30) + "px"
+                }
             }, timeout);
         })
     });
