c2cdab5920d6802e9f5d4f9146693f2adeff5270
GobsRuiz <gbibisruiz@hotmail.com>
2024-11-01 11:43:36 -0300

    Fix: Carousel

--- src/scripts/pages/index/projects/carousel.js
+++ src/scripts/pages/index/projects/carousel.js
@@ -15,14 +15,59 @@ function projectsCarousel()
 
     nextBtn.addEventListener("click", (e) => {
         index = index >= (slides.length - count) ? 0 : index + 1;
+
+        slides[index == 0 ? slides.length - 1 : index - 1].classList.remove("projects__container__carousel__slider__item--show")
+        slides[index].classList.add("projects__container__carousel__slider__item--show")
+
+        if(innerWidth > 568){
+            slides[index + (count - 1)].classList.add("projects__container__carousel__slider__item--show")
+        }
+
+        if(innerWidth > 768){
+            slides[index + (count - 1)].classList.add("projects__container__carousel__slider__item--show")
+        }
+
+        if(innerWidth > 568 && index == 0){
+            slides[0].classList.add("projects__container__carousel__slider__item--show")
+            slides[1].classList.add("projects__container__carousel__slider__item--show")
+
+            // Remove
+            if(innerWidth > 568)
+                slides[slides.length - count].classList.remove("projects__container__carousel__slider__item--show")
+            if(innerWidth > 768)
+                slides[slides.length - (count - 1)].classList.remove("projects__container__carousel__slider__item--show")
+        }
+        
+        // else if(innerWidth < 768){
+        //     slides[index == 0 ? slides.length - 1 : index - 1].classList.remove("projects__container__carousel__slider__item--show")
+
+
+            
+        //     else{
+                // slides[index + 1].classList.add("projects__container__carousel__slider__item--show")
+        //     }
+        // }
+
         changeSlide()
     })
 
+    function setClassShow()
+    {
+        slides[0].classList.add("projects__container__carousel__slider__item--show")
+        
+        if(innerWidth > 568){
+            slides[1].classList.add("projects__container__carousel__slider__item--show")
+        }
+
+        if(innerWidth > 768){
+            slides[2].classList.add("projects__container__carousel__slider__item--show")
+        }
+    }
+    setClassShow()
 
     function setWith(){
         const marginValue = innerWidth > 768 ? 6 : 10
         const calc = ((innerWidth / count) - (40 + marginValue))
-        console.log(calc)
 
         slides.forEach(element => {
             element.style.minWidth = calc + "px"
